b1e7924cca58cf53a4f819501baf1fc7ee96717e
xiongyousheng <327464319@qq.com>
2023-09-08 09:17:31 +0800

    fix:修改登录system配置

--- public/cdn/common/systemInfo.js
+++ public/cdn/common/systemInfo.js
@@ -4,8 +4,8 @@ let systemInfo = {
   indexTitle: 'HZInfo Admin', // 基础页左上角展开显示
   indexLogo: 'H', // 基础页左上角收起显示
   tenantMode: true, // 是否开启租户模式  登录页显示
-  tenantId: '', // 管理组租户编号
-  captchaMode: true, // 是否开启验证码模式
+  tenantId: '000000', // 管理组租户编号
+  captchaMode: false, // 是否开启验证码模式
   // 第三方系统授权地址
   authUrl: 'http://localhost/blade-auth/oauth/render',
   // 流程设计器地址
