1815f4ff55f53c638ad021a6e967b7af2c149d65
xiongyousheng <327464319@qq.com>
2023-09-13 16:53:38 +0800

    fix:删除多余文件

--- src/utils/RenderBase.ts
+++
@@ -1,8 +0,0 @@
-import { useMainStore } from '@/store/index'
-// 定义loader方法，loading改变时，将变量赋值给App.vue的data中的isLoading
-function loader (loading:boolean) {
-  const $store = useMainStore()
-  $store.loadingChange(loading)
-}
-
-export default loader
